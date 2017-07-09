465e24658efdebd0384bd1d48889a93ca6eacd86
Betsy Rowley <BRowley8@gmail.com>
2017-07-09 14:59:27 -0500

    Adds program bar

--- server/public/scripts/controllers/HomeController.js
+++ server/public/scripts/controllers/HomeController.js
@@ -6,4 +6,17 @@ angular
         var vm = this; //controller as reference
 
         vm.hello = YouthService.hello;
+
+        // vm.myInterval = 5000;
+        // vm.noWrapSlides = false;
+        // vm.active = 0;
+        // var slides = vm.slides = [
+        //   {id: 0,
+        //   image: '/images/slide1.jpg',
+        //   title: 'EDUCATION',
+        //   text: 'YouthCare develops the academic and essential skills to' +
+        //           'help youth succeed in school, work and life.'
+        //   }
+        // ];
+
     }]);
