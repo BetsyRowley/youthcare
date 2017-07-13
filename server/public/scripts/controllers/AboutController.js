abcd9fe0d4d276292ccaf703232a34f9126738ab
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2017-07-12 22:29:20 -0500

    images on home screen now displaying more fully

--- server/public/scripts/controllers/AboutController.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('myApp')
-  .controller('AboutController',
-    ['$http', 'YouthService', function($http, YouthService) {
-      //DATA-BINDING VARIABLES
-        var vm = this; //controller as reference
-
-        vm.hello = YouthService.hello;
-    }]);
